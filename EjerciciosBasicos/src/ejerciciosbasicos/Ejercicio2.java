2ca8c9e8705e2683c59ccd68edf4c7c8922a63ae
mdominguezg <mdominguezg@106-06.info2>
2022-09-27 12:35:56 +0200

    commit

---
+++ EjerciciosBasicos/src/ejerciciosbasicos/Ejercicio2.java
@@ -0,0 +1,24 @@
+
+package ejerciciosbasicos;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		//creamos el scanner 
+		Scanner lectura = new Scanner (System.in);
+		// imprimimos en la consola la pregunta para reguistrar una edad	
+		System.out.print("Introduzca su edad: ");
+		//creamos la variable para guardar la edad
+			int num;
+			//guardamos la edad escrita en la variable
+			num= lectura.nextInt();
+			//variable para sumarle uno a la edad del cliente
+			int masUno;
+			masUno= num + 1;
+			//mostramos la respuesta en la consola con la edad que tendrá el cliente el próximo año
+				System.out.print("En su próximo cumpleaños cumplirás: " + masUno);
+		lectura.close();
+	}
+}
